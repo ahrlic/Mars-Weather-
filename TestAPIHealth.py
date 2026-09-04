2bf5a51f3bd7a58785bcdec5b924a507cccfae9b
Ariana Hrlic <ariana.hrlic@gmail.com>
2026-09-03 22:00:46 -0400

    added to main by accident:(

--- TestAPIHealth.py
+++ TestAPIHealth.py
@@ -1,17 +1,22 @@
 import requests
+from requests.exceptions import Timeout
 
 def TestAPIHealth():
 
-    
-    response = requests.get("https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0", timeout=2) 
+  try: 
+    response = requests.get("https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0", timeout=2).json() 
 
-    data = response.json()
+    total_records = len(response)
 
     if response:
-      print("Success!")
+      print("Active")
     else:
       response.encoding = "uft-8"
       print("Run Instance:", response.headers['Date'], "\n")
+      print(f"Total records: {total_records}", "\n")  
       raise Exception(f"Non-success status code: {response.status_code}")
+
+  except Timeout:
+     print("The request timed out")
         
 TestAPIHealth()
