f0c60bff08f1dbea6135fa6aeae8bef52cb365a3
Ariana Hrlic <ariana.hrlic@gmail.com>
2026-09-04 17:45:14 -0400

    Add files via upload

--- TestAPIHealth.py
+++ TestAPIHealth.py
@@ -1,22 +1,36 @@
 import requests
 from requests.exceptions import Timeout
 
+
 def TestAPIHealth():
 
-  try: 
-    response = requests.get("https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0", timeout=2).json() 
+    try:
+        response = requests.get(
+            "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0",
+            timeout=2,
+        )
 
-    total_records = len(response)
+        if response:
+            print("Active")
 
-    if response:
-      print("Active")
-    else:
-      response.encoding = "uft-8"
-      print("Run Instance:", response.headers['Date'], "\n")
-      print(f"Total records: {total_records}", "\n")  
-      raise Exception(f"Non-success status code: {response.status_code}")
+        else:
 
-  except Timeout:
-     print("The request timed out")
-        
+            response.encoding = "utf-8"
+            print("Run Instance:", response.headers["Date"], "\n")
+            print(response.elapsed.total_seconds())
+
+        with open("logAPIError", "a") as f:
+            f.write(
+                f"\n"
+                f"Run Instance: {response.headers.get('Date', 'N/A')}\n"
+                f"Time Elapsed: {response.elapsed.total_seconds()}\n"
+                f"Status Code: {response.status_code}\n"
+            )
+
+        raise Exception(f"Non-success status code: {response.status_code}")
+
+    except Timeout:
+        print("The request timed out")
+
+
 TestAPIHealth()
