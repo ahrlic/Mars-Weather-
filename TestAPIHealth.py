2468a79b411a763512b74df3ccd45e626ba98314
Ariana Hrlic <ariana.hrlic@gmail.com>
2026-09-05 17:35:59 -0400

    issue: path to file for subprocess not found

--- TestAPIHealth.py
+++ TestAPIHealth.py
@@ -1,36 +1,48 @@
 import requests
 from requests.exceptions import Timeout
 
+# Ariana Hrlic
+# 2026/09/05
+#Automate API Health check before running df
 
-def TestAPIHealth():
+class TestAPIHealth:
+    def TestAPIHealth():
 
-    try:
-        response = requests.get(
-            "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0",
-            timeout=2,
-        )
-
-        if response:
-            print("Active")
-
-        else:
-
-            response.encoding = "utf-8"
-            print("Run Instance:", response.headers["Date"], "\n")
-            print(response.elapsed.total_seconds())
-
-        with open("logAPIError", "a") as f:
-            f.write(
-                f"\n"
-                f"Run Instance: {response.headers.get('Date', 'N/A')}\n"
-                f"Time Elapsed: {response.elapsed.total_seconds()}\n"
-                f"Status Code: {response.status_code}\n"
+        try:
+            response = requests.get(
+                "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0",
+                timeout=2,
             )
 
-        raise Exception(f"Non-success status code: {response.status_code}")
+            if response:
+                with open("logAPIError", "a") as f:
+                    f.write(
+                        f"\n"
+                        f"Run Instance: {response.headers.get('Date', 'N/A')}\n"
+                        f"Time Elapsed: {response.elapsed.total_seconds()}\n"
+                        f"Active"
+                    )
+              
+                return True
 
-    except Timeout:
-        print("The request timed out")
+            else:
 
+                response.encoding = "utf-8"
+                print("Run Instance:", response.headers["Date"], "\n")
+                print(response.elapsed.total_seconds())
 
+            with open("logAPIError", "a") as f:
+                f.write(
+                    f"\n"
+                    f"Run Instance: {response.headers.get('Date', 'N/A')}\n"
+                    f"Time Elapsed: {response.elapsed.total_seconds()}\n"
+                    f"Status Code: {response.status_code}\n"
+                )
+
+                return False
+            raise Exception(f"Non-success status code: {response.status_code}")
+
+        except Timeout:
+            print("The request timed out")
+        
 TestAPIHealth()
